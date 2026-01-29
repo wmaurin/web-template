848a1e41c608c76c2e09ac1c30598d8ca9351b8a
wmaurin <dev.maurin@gmail.com>
2026-01-29 20:36:12 +0100

    chore: install geist font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import '@fontsource/geist/400.css';
+import '@fontsource/geist/500.css';
+import '@fontsource/geist/600.css';
+import '@fontsource/geist/700.css';
 import './index.css';
 import App from './App.tsx';
 
